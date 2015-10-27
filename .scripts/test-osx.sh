4e6bca487ad3094250e2e13725f0e18c114b45dc
Daniel Thorpe <danthorpe@me.com>
2015-10-27 13:37:52 +0000

    [VCD-4]: Moving fastlane crash reporting to update bundler step.

--- .scripts/test-osx.sh
+++ .scripts/test-osx.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
-bundle exec fastlane enable_crash_reporting
 bundle exec fastlane mac test
--- .scripts/update-bundler.sh
+++ .scripts/update-bundler.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 source /usr/local/opt/chruby/share/chruby/chruby.sh
 chruby ruby
-bundle update
+bundle update && fastlane enable_crash_reporting
