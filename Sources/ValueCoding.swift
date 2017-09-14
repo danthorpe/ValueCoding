22a0c0912a5425b8b6c000066e4f3707cf2075be
Daniel Thorpe <danthorpe@me.com>
2017-09-14 21:07:53 +0100

    [VCD-28]: Updates file headers

--- Sources/ValueCoding.swift
+++ Sources/ValueCoding.swift
@@ -1,9 +1,11 @@
 //
-//  ValueCoding.swift
-//  ValueCoding
+// ValueCoding
+// File created on 11/10/2015.
 //
-//  Created by Daniel Thorpe on 11/10/2015.
+// Copyright (c) 2015-2017 Daniel Thorpe
 //
+// ValueCoding is licensed under the MIT License. Read the full license at
+// https://github.com/danthorpe/ValueCoding/blob/master/LICENSE
 //
 
 import Foundation
